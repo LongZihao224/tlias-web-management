f5f3ba8971dd8d6c7189c3fa3544d1041005a35e
LongZihao224 <783021227@qq.com>
2023-10-29 19:47:20 +0800

    初始化项目

--- tlias-web-management/src/main/java/com/itheima/exception/GlobalExceptionHandle.java
+++ tlias-web-management/src/main/java/com/itheima/exception/GlobalExceptionHandle.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 全局异常处理器
- * 1. 编写异常处理类，使用@ControllerAdvice和@ExceptionHandler注解
+ * 1. 编写异常处理类，使用@RestControllerAdvice和@ExceptionHandler注解
  * 2. 在异常处理类中，编写异常处理方法
  * 3. 在异常处理方法上，编写异常处理逻辑
  * 4. 在异常处理方法上，使用@ResponseBody注解，将异常处理结果响应给客户端
